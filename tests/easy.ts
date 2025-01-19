5485e86842423f963c0e55e17df716a7cb1b332a
Sean Zellmer <sean@lejeunerenard.com>
2025-01-18 20:25:29 -0600

    chore: Add test for easy's replays.list()

--- tests/easy.ts
+++ tests/easy.ts
@@ -2,7 +2,7 @@ import test from 'brittle'
 import nock from 'nock'
 import { Arbitrary, FastCheck } from 'effect'
 import { HttpClientResponse } from '@effect/platform'
-import { Replay } from '../src/schema/replay'
+import { Replay, ReplaySummary } from '../src/schema/replay'
 import { getClient } from '../src/easy'
 import { API_URL } from '../src/constants'
 
@@ -22,4 +22,23 @@ test('easy - get replay', async (t) => {
 
   const replay = await client.replays.get('123')
   t.ok(scope.isDone())
+  t.is(JSON.stringify(replay), JSON.stringify(replayMock))
+})
+
+test('easy - list replay', async (t) => {
+  const replayMocks = FastCheck.sample(Arbitrary.make(ReplaySummary), 4)
+  const scope = nock(API_URL)
+    .get('/replays')
+    .times(1)
+    .reply(200, { list: replayMocks })
+
+  const client = getClient('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+
+  const replays = await client.replays.list()
+  t.ok(scope.isDone())
+  for (let i = 0; i < replays.length; i++) {
+    const replay = replays[i]
+    const replayMock = replayMocks[i]
+    t.is(JSON.stringify(replay), JSON.stringify(replayMock))
+  }
 })
